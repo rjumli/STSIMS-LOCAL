d975c0b48e90c0052b8b678101af8d3ab6c3ef04
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-11-28 14:03:17 +0800

    asda

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,30 +14,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \DB::table('users')->insert([
-        //     'username' => 'administrator',
-        //     'email' => 'kradjumli@gmail.com',
-        //     'password' => bcrypt('123456789'),
-        //     'role' => 'Administrator',
-        //     'avatar' => 'administrator.png',
-        //     'is_active' => 0,
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        //     'email_verified_at' => now(),
-        // ]);
+        \DB::table('users')->insert([
+            'username' => 'administrator',
+            'email' => 'kradjumli@gmail.com',
+            'password' => bcrypt('123456789'),
+            'role' => 'Administrator',
+            'avatar' => 'administrator.png',
+            'is_active' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'email_verified_at' => now(),
+        ]);
 
-        // \DB::table('user_profiles')->insert([
-        //     'firstname' => 'Ra-ouf',
-        //     'lastname' => 'Jumli',
-        //     'middlename' => 'Indanan',
-        //     'gender' => 'Male',
-        //     'user_id' => 1,
-        //     'mobile' => '09171531652',
-        //     'profileable_type' => 'App\Models\LocationRegion',
-        //     'profileable_id' =>'090000000',
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        // ]);
+        \DB::table('user_profiles')->insert([
+            'firstname' => 'Ra-ouf',
+            'lastname' => 'Jumli',
+            'middlename' => 'Indanan',
+            'gender' => 'Male',
+            'user_id' => 1,
+            'mobile' => '09171531652',
+            'profileable_type' => 'App\Models\LocationRegion',
+            'profileable_id' =>'090000000',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
        
         // $this->call(LocationRegionsTableSeeder::class);
         // $this->call(LocationProvincesTableSeeder::class);
@@ -51,8 +51,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(SchoolCampusesTableSeeder::class);
         // $this->call(ListCoursesTableSeeder::class);
         // $this->call(ListStatusesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(UserProfilesTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(UserProfilesTableSeeder::class);
         
         // $this->call(ScholarsTableSeeder::class);
         // $this->call(ScholarAddressesTableSeeder::class);
